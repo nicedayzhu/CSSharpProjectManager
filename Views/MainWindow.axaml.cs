e30acadd2184cdc5c35718d9472e9b5470e3c7ab
niceday_zhu <wobudangxuezhang@gmail.com>
2025-07-16 21:58:25 +0800

    [init] Initial code for CSSharpProjectManager
    Signed-off-by: niceday_zhu <wobudangxuezhang@gmail.com>

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CSSharpProjectManager.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
